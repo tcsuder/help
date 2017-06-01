603f248c0f721c027bb7905087f1ece7a74ef92b
tyler suderman <tcsuder@gmail.com>
2017-06-01 15:45:54 -0700

    set up presumable structure for state - does not work

--- src/components/Clock.jsx
+++ src/components/Clock.jsx
@@ -1,9 +1,33 @@
 import React from "react";
 
-function Clock(props){
-  return (
-    <h2>It is {props.time}</h2>
-  );
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {time: new Date().toLocaleTimeString()};
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    });
+  }
+
+  render() {
+    return (
+      <h2>It is {this.state.time}</h2>
+    );
+  }
 }
 
 module.exports = Clock;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import Clock from "./Clock";
 
-function Header(props) {
+function Header() {
   return (
     <div>
       <h1>Help Queue</h1>
-      <Clock time={props.time}/>
+      <Clock/>
     </div>
   );
 }
