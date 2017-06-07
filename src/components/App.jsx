e75558064c294851f5232b70fd7a7de3700c8517
tyler suderman <tcsuder@gmail.com>
2017-06-07 11:19:57 -0700

    set up app.jsx with basic ticket route

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,21 @@
 import React from "react";
+import {
+  BrowserRouter,
+  Route
+} from "react-router-dom";
 import Header from "./Header";
 import Ticket from "./Ticket";
 
 function App(props) {
   return (
-    <div>
-      <Header time={props.time}/>
-      <Ticket location="3a" name="Thato and Haley" timeWaiting="4 mins"/>
-      <Ticket location="4b" name="Sleater and Kinney" timeWaiting="15 mins"/>
-    </div>
+    <BrowserRouter>
+      <div>
+        <Header time={props.time}/>
+        <Route path="tickets" component={Ticket}/>
+        // <Ticket location="3a" name="Thato and Haley" timeWaiting="4 mins"/>
+        // <Ticket location="4b" name="Sleater and Kinney" timeWaiting="15 mins"/>
+      </div>
+    </BrowserRouter>
   );
 }
 
