24e8c84b8d48888046b9a694f94fb68898256e90
tyler suderman <tcsuder@gmail.com>
2017-06-07 15:09:37 -0700

    re-add header component without clock

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,7 +5,7 @@ import Ticket from "./Ticket";
 function App(props) {
   return (
     <div>
-      <h1>Help Queue</h1>
+      <Header/>
       <Ticket location="3a" name="Thato and Haley" timeWaiting="4 mins"/>
       <Ticket location="4b" name="Sleater and Kinney" timeWaiting="15 mins"/>
     </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,9 @@
 import React from "react";
-import Clock from "./Clock";
 
 function Header(props) {
   return (
     <div>
       <h1>Help Queue</h1>
-      <Clock time={props.time}/>
     </div>
   );
 }
