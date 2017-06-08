46fc4459a8a2b40830d40531a260de311c10093c
tyler suderman <tcsuder@gmail.com>
2017-06-08 11:31:00 -0700

    set up webpack for HMR

--- webpack.config.js
+++ webpack.config.js
@@ -2,21 +2,37 @@ const { webpack } = require("webpack");
 const { resolve } = require('path');
 
 module.exports = {
-  entry: resolve(__dirname, "src") + "/index.jsx",
+  // root JS file... or starting point for the app
+  entry: [
+    resolve(__dirname, "src") + "/index.jsx",
+    resolve(__dirname) + "/index.html",
+  ],
+  // the location and file name of our js files after being transpiled and concatinated
   output: {
     path: resolve(__dirname, "build"),
     filename: "app.bundle.js"
   },
+  // what kind of transformations do we want webpack to enact on our code?
   module: {
-    loaders: [{
-      test: /\.jsx?$/,
-      loader: "babel-loader",
-      exclude: /node_modules/,
-      query: {
-        presets: ['es2015', 'react']
+    rules: [
+      {
+        // a regular expression that searches for files with a certain syntactical pattern
+        test: /\.jsx?$/,
+        // exclude files that match the test but should not be transpiled
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        query: {
+          presets: ['es2015', 'react']
+        }
+      },
+      {
+        test: /\.html$/,
+        // loader originally included additional substring '!extract-loader!html-loader'
+        loader: 'file-loader?name=[path][name].[ext]'
       }
-    }]
+    ]
   },
+  // resolves file names without js or jsx extensions in component imports
   resolve: {
     extensions: ['.js', '.jsx']
   }
